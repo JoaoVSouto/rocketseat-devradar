51c1f6e2915bac286467bd954b794a5419e82bd4
JoaoVSouto <joaovitorss99@hotmail.com>
2020-01-14 19:39:14 -0300

    [BACKEND] Configs schema to receive geolocation

--- backend/src/app/controllers/DevController.js
+++ backend/src/app/controllers/DevController.js
@@ -4,7 +4,7 @@ import Dev from '../models/Dev';
 
 class DevController {
   async store(req, res) {
-    const { github_username, techs } = req.body;
+    const { github_username, techs, latitude, longitude } = req.body;
 
     const response = await axios.get(
       `https://api.github.com/users/${github_username}`
@@ -15,12 +15,18 @@ class DevController {
 
     const techsArray = techs.split(',').map(tech => tech.trim());
 
+    const location = {
+      type: 'Point',
+      coordinates: [longitude, latitude],
+    };
+
     const dev = await Dev.create({
       github_username,
       name,
       avatar_url,
       bio,
       techs: techsArray,
+      location,
     });
 
     return res.json(dev);
