4ba1287f4d8c068a1c547060f551060a860f3df6
JoaoVSouto <joaovitorss99@hotmail.com>
2020-01-14 18:37:25 -0300

    [BACKEND] Sets up routes file

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,19 +2,20 @@ import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
 
+import routes from './routes';
+
 dotenv.config();
 
 const { PORT, DB_CONN } = process.env;
 
-const app = express();
+const server = express();
 
 mongoose
   .connect(DB_CONN, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to database!'))
   .catch(err => console.error('Error in connecting to database', err));
 
-app.use(express.json());
+server.use(express.json());
+server.use(routes);
 
-app.post('/', (req, res) => res.json({ ok: true }));
-
-app.listen(PORT, () => console.log(`Server listening on ${PORT}...`));
+server.listen(PORT, () => console.log(`Server listening on ${PORT}...`));
