df5c3d8b4e3af3a0275b24e0aaf2e0689d512321
JoaoVSouto <joaovitorss99@hotmail.com>
2020-01-14 18:31:05 -0300

    [BACKEND] Adds connection to database

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,20 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import mongoose from 'mongoose';
 
 dotenv.config();
 
+const { PORT, DB_CONN } = process.env;
+
 const app = express();
+
+mongoose
+  .connect(DB_CONN, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to database!'))
+  .catch(err => console.error('Error in connecting to database', err));
 
 app.use(express.json());
 
 app.post('/', (req, res) => res.json({ ok: true }));
-
-const { PORT } = process.env;
 
 app.listen(PORT, () => console.log(`Server listening on ${PORT}...`));
