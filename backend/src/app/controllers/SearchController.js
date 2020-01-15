d83bf74c8f6b2d39e95400a12df21f4c153e0ba4
JoaoVSouto <joaovitorss99@hotmail.com>
2020-01-14 21:46:36 -0300

    [BACKEND] Adds dev's search by techs and distance

--- backend/src/app/controllers/SearchController.js
+++ backend/src/app/controllers/SearchController.js
@@ -8,9 +8,22 @@ class SearchController {
 
     const techsArray = parseStringAsArray(techs);
 
-    console.log(techsArray);
+    const devs = await Dev.find({
+      techs: {
+        $in: techsArray,
+      },
+      location: {
+        $near: {
+          $geometry: {
+            type: 'Point',
+            coordinates: [longitude, latitude],
+          },
+          $maxDistance: 10000,
+        },
+      },
+    });
 
-    return res.json({ ok: true });
+    return res.json(devs);
   }
 }
 
