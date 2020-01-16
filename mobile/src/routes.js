6092050620d197338db4249718bef21723198d95
JoaoVSouto <joaovitorss99@hotmail.com>
2020-01-16 09:39:59 -0300

    [MOBILE] Styles top bar

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -5,20 +5,30 @@ import Main from './pages/Main';
 import Profile from './pages/Profile';
 
 const Routes = createAppContainer(
-  createStackNavigator({
-    Main: {
-      screen: Main,
-      navigationOptions: {
-        title: 'DevRadar',
+  createStackNavigator(
+    {
+      Main: {
+        screen: Main,
+        navigationOptions: {
+          title: 'DevRadar',
+        },
+      },
+      Profile: {
+        screen: Profile,
+        navigationOptions: {
+          title: 'Perfil no Github',
+        },
       },
     },
-    Profile: {
-      screen: Profile,
-      navigationOptions: {
-        title: 'Perfil no Github',
+    {
+      defaultNavigationOptions: {
+        headerTintColor: '#fff',
+        headerStyle: {
+          backgroundColor: '#7d40e7',
+        },
       },
-    },
-  })
+    }
+  )
 );
 
 export default Routes;
